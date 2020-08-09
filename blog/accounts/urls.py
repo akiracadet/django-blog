1514415b7c8f7738514bdeb206ff5fd46b8bc099
Mavis Vermillion <mavis.vermillion.ichi@gmail.com>
2020-08-09 21:21:36 +0800

    Implemented basic registration

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
+from accounts.views import RegistrationView
 
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegistrationView.as_view(template_name='accounts/registration.html'), name='registration'),
 ]
--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,1 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from django.views import View
+from accounts.forms import RegistrationForm
+
+
+class RegistrationView(View):
+    template_name = None
+
+    def get(self, request):
+        context = {}
+
+        if request.user.is_authenticated:
+            logout(request)
+
+        context = {
+            'form': RegistrationForm()
+        }
+
+        return render(request, self.template_name, context)
+
+
+    def post(self, request):
+        context = {}
+
+        if request.user.is_authenticated:
+            logout(request)
+
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+            return redirect('home')
+
+        context['form'] = form
+
+        return render(request, self.template_name, context)
