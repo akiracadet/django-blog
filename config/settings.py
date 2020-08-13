4a2dc12aaca330354972c779ed89a0e8f5419732
Mavis Vermillion <mavis.vermillion.ichi@gmail.com>
2020-08-13 23:49:50 +0800

    another update

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mavis-django-app.herokuapp.com']
+ALLOWED_HOSTS = ['mavis-django-app.herokuapp.com', 'gmail.com', 'localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
